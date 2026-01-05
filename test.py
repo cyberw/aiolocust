43fc5ae767105bdda196e9daf1b6323c6be1654b
Lars Holmberg <lars.holmberg@redshirt.se>
2026-01-05 13:36:27 +0100

    move stuff into library

--- test.py
+++ test.py
@@ -1,44 +1,6 @@
 import asyncio
-import time
 
-from aiohttp import ClientSession
-from aiohttp.client import _RequestContextManager
-
-
-class LocustRequestContextManager:
-    def __init__(self, response_cm: _RequestContextManager):
-        self.response_cm = response_cm
-
-    async def __aenter__(self):
-        self.start_time = time.perf_counter()
-        resp = await self.response_cm.__aenter__()
-        self.url = self.response_cm._resp.url
-        self.ttfb = time.perf_counter() - self.start_time
-        await resp.read()
-        self.ttlb = time.perf_counter() - self.start_time
-        return resp
-
-    async def __aexit__(self, exc_type, exc_val, exc_tb):
-        suppress = await self.response_cm.__aexit__(exc_type, exc_val, exc_tb)
-        print(self.url, self.ttfb, self.ttlb)
-        if exc_type is not None:
-            import traceback
-
-            traceback.print_exception(exc_type, exc_val, exc_tb)
-            # Suppress exceptions raised inside the `async with` block so
-            # callers of `LocustClientSession.get()` don't see them.
-            return True
-
-        return suppress
-
-
-class LocustClientSession(ClientSession):
-    # explicitly declare this to get the correct return type
-    async def __aenter__(self) -> LocustClientSession:
-        return self
-
-    def get(self, url, **kwargs) -> LocustRequestContextManager:
-        return LocustRequestContextManager(super().get(url, **kwargs))
+from aiolocust import LocustClientSession
 
 
 async def user(client: LocustClientSession):
