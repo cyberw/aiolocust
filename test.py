8554b01389ca6ae9def3de27b1eb589331171005
Lars Holmberg <lars.holmberg@redshirt.se>
2026-01-05 14:43:49 +0100

    better examples

--- test.py
+++ test.py
@@ -5,11 +5,11 @@ from aiolocust import LocustClientSession, main
 
 
 async def user(client: LocustClientSession):
-    for i in range(5000000):
-        _ = i + 1  # really important computation
+    for i in range(20000000):
+        _ = i + 1  # use some cpu!
     async with client.get("https://locust.io/static/img/screenshot_2.31.3-dev_dark.png") as resp:
         assert resp.status == 200
     await asyncio.sleep(random.uniform(0.1, 1.0))
 
 
-asyncio.run(main(user, 4))
+asyncio.run(main(user, 16))
