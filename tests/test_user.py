754479ff366623f66c6fe578d536e0c88e763d50
Lars Holmberg <lars.holmberg@redshirt.se>
2026-01-07 11:40:06 +0100

    add another test case

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,14 +1,27 @@
 import pytest
+from aiohttp.client_exceptions import ClientResponseError
 
 from aiolocust import LocustClientSession
 
 
-async def example_user(client: LocustClientSession):
-    async with client.get("https://locust.io/static/img/screenshot_2.31.3-dev_dark.png") as resp:
-        assert resp.status == 200
+@pytest.mark.asyncio
+async def test_session():
+    async def _(client: LocustClientSession):
+        async with client.get(
+            "https://locust.io/static/img/screenshot_2.31.3-dev_dark.png"
+        ) as resp:
+            assert resp.status == 200
+
+    async with LocustClientSession() as client:
+        await _(client)
 
 
 @pytest.mark.asyncio
-async def test_session():
-    async with LocustClientSession() as client:
-        await example_user(client)
+async def test_raise_for_status():
+    async def _(client: LocustClientSession):
+        async with client.get("http://localhost/README2.md", raise_for_status=True) as resp:
+            pass
+
+    with pytest.raises(ClientResponseError):
+        async with LocustClientSession() as client:
+            await _(client)
