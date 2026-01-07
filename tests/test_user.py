bc7ff91740b00fddc84587a04e1e47a36bc0b4ae
Lars Holmberg <lars.holmberg@redshirt.se>
2026-01-07 15:59:44 +0100

    Use pytest-httpserver for tests

--- tests/test_user.py
+++ tests/test_user.py
@@ -5,11 +5,11 @@ from aiolocust import LocustClientSession
 
 
 @pytest.mark.asyncio
-async def test_session():
+async def test_get(httpserver):
+    httpserver.expect_request("/").respond_with_data("")
+
     async def _(client: LocustClientSession):
-        async with client.get(
-            "https://locust.io/static/img/screenshot_2.31.3-dev_dark.png"
-        ) as resp:
+        async with client.get(httpserver.url_for("/")) as resp:
             assert resp.status == 200
 
     async with LocustClientSession() as client:
@@ -17,9 +17,23 @@ async def test_session():
 
 
 @pytest.mark.asyncio
-async def test_raise_for_status():
+async def test_post(httpserver):
+    httpserver.expect_request("/", method="POST").respond_with_data("")
+
     async def _(client: LocustClientSession):
-        async with client.get("http://localhost/README2.md", raise_for_status=True) as resp:
+        async with client.get(httpserver.url_for("/")) as resp:
+            assert resp.status == 200
+
+    async with LocustClientSession() as client:
+        await _(client)
+
+
+@pytest.mark.asyncio
+async def test_raise_for_status(httpserver):
+    httpserver.expect_request("/README2.md").respond_with_data("", status=404)
+
+    async def _(client: LocustClientSession):
+        async with client.get(httpserver.url_for("/README2.md"), raise_for_status=True) as resp:
             pass
 
     with pytest.raises(ClientResponseError):
