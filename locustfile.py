bb8c32531f76dd9f456ab1ce1d8c3e88e459979a
Lars Holmberg <lars.holmberg@redshirt.se>
2026-01-08 18:40:50 +0100

    GH actions: Add full perf test to build

--- locustfile.py
+++ locustfile.py
@@ -12,14 +12,14 @@ def busy_loop(seconds: float):
 
 
 async def user(client: LocustClientSession):
-    async with client.get("http://localhost/README.md") as resp:
+    async with client.get("http://localhost:8080/index.html") as resp:
         assert resp.status == 200
-    # raise exception (interrupt user) on bad status
-    async with client.get("http://localhost/README2.md", raise_for_status=True) as resp:
-        pass
-    async with client.get("http://localhost/uv.lock") as resp:
-        # idk exactly how big uv.lock is, but its gotta be big
-        assert resp.content_length and resp.content_length > 10000
+    # # raise exception (interrupt user) on bad status
+    # async with client.get("http://localhost/README2.md", raise_for_status=True) as resp:
+    #     pass
+    # async with client.get("http://localhost:8080/uv.lock") as resp:
+    #     # idk exactly how big uv.lock is, but its gotta be big
+    #     assert resp.content_length and resp.content_length > 10000
     # If you want to make things slower/experiment with loadgen performance:
     # busy_loop(0.1)
     # await asyncio.sleep(0.1)
