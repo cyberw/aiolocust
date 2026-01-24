65acd61a1780029a6b73292e3fe99d0af113fd22
Lars Holmberg <lars.holmberg@redshirt.se>
2026-01-25 00:12:06 +0100

    add test for runner

---
+++ tests/test_runner.py
@@ -0,0 +1,20 @@
+import asyncio
+
+import pytest
+
+from aiolocust.runner import LocustClientSession, main
+
+
+@pytest.mark.asyncio
+async def test_runner(http_server, capfd):  # noqa: ARG001
+    async def run(client: LocustClientSession):
+        await asyncio.sleep(1)
+        print("hello")
+        async with client.get("http://localhost:8081/") as resp:
+            pass
+
+    await main(run, 1, 1)
+    out, err = capfd.readouterr()
+    assert " http://localhost:8081/ " in out
+    assert "0 (0.0%)" in out
+    assert err == ""
