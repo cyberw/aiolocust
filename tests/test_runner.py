27b6af9a67d8ff8dfe86934c90a72b09e94ed658
Lars Holmberg <lars.holmberg@redshirt.se>
2026-01-25 19:56:54 +0100

    revert small accidental change in test

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -7,13 +7,13 @@ from aiolocust.runner import LocustClientSession, run_test
 
 @pytest.mark.asyncio
 async def test_runner(http_server, capfd):  # noqa: ARG001
-    async def user(client: LocustClientSession):
+    async def run(client: LocustClientSession):
         await asyncio.sleep(1)
         print("hello")
         async with client.get("http://localhost:8081/") as resp:
             pass
 
-    await run_test(user, 1, 2)
+    await run_test(run, 1, 2)
     out, err = capfd.readouterr()
     assert err == ""
     assert " http://localhost:8081/ " in out
