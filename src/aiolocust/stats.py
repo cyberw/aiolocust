72ba8e43b15dc06c0bb2f50707c3744e9530354b
Lars Holmberg <lars.holmberg@redshirt.se>
2026-01-27 23:40:53 +0100

    Calculate request_rate correctly. Help pyright understand that seconds_range will always be set.

--- src/aiolocust/stats.py
+++ src/aiolocust/stats.py
@@ -37,6 +37,7 @@ def print_table(summary=False):
     total_max_ttlb = 0
     total_count = 0
     total_errorcount = 0
+    seconds_range = 0  # be calm, pyright...
     table = Table(show_edge=False)
     table.add_column("Name", max_width=30)
     table.add_column("Count", justify="right")
@@ -62,7 +63,7 @@ def print_table(summary=False):
         error_percentage = 100 * errorcount / count if count else 0
         avg_ttlb_ms = 1000 * sum_ttlb / count if count else 0
         max_ttlb_ms = 1000 * max_ttlb
-        request_rate = count / seconds_range
+        request_rate = count / elapsed if summary else count / seconds_range
         table.add_row(
             url,
             str(count),
@@ -87,14 +88,15 @@ def print_table(summary=False):
             "",
         )
     else:
+        total_rate = total_count / elapsed if summary else total_count / seconds_range
         table.add_row(
             "Total",
             str(total_count),
             f"{total_errorcount} ({100 * total_errorcount / total_count:2.1f}%)",
             f"{1000 * total_ttlb / total_count:4.1f}ms",
             f"{1000 * total_max_ttlb:4.1f}ms",
-            f"{total_count / seconds_range:.2f}/s",
+            f"{total_rate:.2f}/s",
         )
-        table.add_row("Run time", str(elapsed))
+        table.add_row("Run time", f"{elapsed:.1f}")
     print()
     console.print(table)
