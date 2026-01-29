8e59f1023f0f5fbb1021aa9854571bc1d178eba1
Lars Holmberg <lars.holmberg@redshirt.se>
2026-01-29 23:40:09 +0100

    Stabilise test_runner, particularly on windows

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -12,20 +12,21 @@ async def test_runner(http_server, capfd):  # noqa: ARG001
             pass
 
     r = Runner()
-    await r.run_test(run, 1, 5)
+    await r.run_test(run, 1, 4)
     out, err = capfd.readouterr()
     assert err == ""
     print(out)
     # first print will happen after a request has been created, but it is not yet in the window
     assert "http://localhost:8081/ │     0" in out
-    # second print should have two requsts in the window
-    assert_search(" http://localhost:8081/ │     2|3", out)
+    # second print should have two requsts in the window, but Windows on GH is randomly slow
+    assert_search(" http://localhost:8081/ │     1|2|3", out)
 
-    r.stats.print_table(True)  # call to print summary is made in main, so we do it manually here
+    # call to print summary is normally made in main, so we have to do it manually here
+    r.stats.print_table(True)
 
     out, err = capfd.readouterr()
     print(out)
     assert err == ""
     assert "Summary" in out
-    assert_search(" http://localhost:8081/ │     5|6", out)
+    assert_search(" http://localhost:8081/ │     4|5", out)
     assert "0 (0.0%)" in out
