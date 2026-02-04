466e8b54d4cbc8920a6ff85205334472fd06b76b
Lars Holmberg <lars.holmberg@redshirt.se>
2026-02-04 11:26:58 +0100

    Improve test_runner case, add a failing request to it as well.

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -10,22 +10,13 @@ async def test_runner(http_server, capfd):  # noqa: ARG001
         await asyncio.sleep(1)
         async with client.get("http://localhost:8081/") as resp:
             pass
+        async with client.get("http://localhost:8081/") as resp:
+            assert "foo" in await resp.text()
 
     r = Runner()
-    await r.run_test(run, 1, 5)
+    await r.run_test(run, 1, 3)
     out, err = capfd.readouterr()
     assert err == ""
     print(out)
-    # second print should have two requsts in the window, but Windows on GH is randomly slow
-    assert_search(" http://localhost:8081/ │     1|2|3", out)
-
-    # Summary not yet implemented for OTEL
-    # # call to print summary is normally made in main, so we have to do it manually here
-    r.stats.print_table(True)
-
-    # out, err = capfd.readouterr()
-    # print(out)
-    # assert err == ""
-    # assert "Summary" in out
-    # assert_search(" http://localhost:8081/ │     4|5|6", out)
-    # assert "0 (0.0%)" in out
+    assert "Summary" in out
+    assert_search(r" http://localhost:8081/ │[ ]+[468] .* \(50.0%\)", out)
