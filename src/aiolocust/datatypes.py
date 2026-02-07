45b27886b479242c8b1bc5e0a77b5d5fc714f83c
Lars Holmberg <lars.holmberg@redshirt.se>
2026-02-07 15:15:45 +0100

    Refactor RequestEntry to simplify print_table

--- src/aiolocust/datatypes.py
+++ src/aiolocust/datatypes.py
@@ -11,8 +11,40 @@ class Request:
 
 @dataclass(slots=True)
 class RequestEntry:
-    count: int
-    errorcount: int
-    sum_ttfb: float
-    sum_ttlb: float
-    max_ttlb: float
+    count: int = 0
+    errorcount: int = 0
+    sum_ttlb: float = 0.0
+    max_ttlb: float = 0.0
+
+    def add_datapoint(self, count: int, errorcount: int, sum_ttlb: float, max_ttlb: float):
+        self.count += count
+        self.errorcount += errorcount
+        self.sum_ttlb += sum_ttlb
+        self.max_ttlb = max(self.max_ttlb, max_ttlb)
+
+    def __iadd__(self, other: RequestEntry):
+        if isinstance(other, RequestEntry):
+            self.count += other.count
+            self.errorcount += other.errorcount
+            self.sum_ttlb += other.sum_ttlb
+            self.max_ttlb = max(self.max_ttlb, other.max_ttlb)
+            return self
+
+    def rate(self, start, end) -> float:
+        return self.count / (end - start)
+
+    @property
+    def avg_ttlb(self) -> float:
+        return self.sum_ttlb / self.count if self.count > 0 else 0.0
+
+    @property
+    def avg_ttlb_ms(self) -> float:
+        return self.avg_ttlb * 1000
+
+    @property
+    def max_ttlb_ms(self) -> float:
+        return self.max_ttlb * 1000
+
+    @property
+    def error_percentage(self) -> float:
+        return self.errorcount / self.count * 100.0 if self.count > 0 else 0.0
