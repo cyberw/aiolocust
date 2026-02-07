d3355ee01fe749fa52b47e044d9c162e5f5845f2
Lars Holmberg <lars.holmberg@redshirt.se>
2026-02-07 17:32:36 +0100

    Split print_table into aggregation (get_table) and printing

--- src/aiolocust/stats.py
+++ src/aiolocust/stats.py
@@ -39,6 +39,17 @@ meter = metrics.get_meter("locust")
 ttlb_histogram = meter.create_histogram("http.client.duration")
 
 
+def make_row(name: str, re: RequestEntry, start, end) -> list[str]:
+    return [
+        name,
+        str(re.count),
+        f"{re.errorcount} ({re.error_percentage:2.1f}%)",
+        f"{re.avg_ttlb_ms:4.1f}ms",
+        f"{re.max_ttlb_ms:4.1f}ms",
+        f"{re.rate(start, end):.2f}/s",
+    ]
+
+
 class Stats:
     def __init__(self, console: Console | None = None):
         self._console = console if console else Console()
@@ -88,14 +99,9 @@ class Stats:
 
         return entries
 
-    def print_table(self, final_summary=False):
-        table = Table(show_edge=False)
-        table.add_column("Name", max_width=30)
-        table.add_column("Count", justify="right")
-        table.add_column("Failures", justify="right")
-        table.add_column("Avg", justify="right")
-        table.add_column("Max", justify="right")
-        table.add_column("Rate", justify="right")
+    def get_table(self, final_summary) -> list[list[str]]:
+        table: list[list[str]] = []
+        summary_table: list[list[str]] = []
         now = time.time()
 
         entries = self._get_entries()
@@ -104,61 +110,48 @@ class Stats:
         for url, re in entries.items():
             self.aggregate[url] += re
             total += re
-            if not final_summary:
-                table.add_row(
-                    url,
-                    str(re.count),
-                    f"{re.errorcount} ({re.error_percentage:2.1f}%)",
-                    f"{re.avg_ttlb_ms:4.1f}ms",
-                    f"{re.max_ttlb_ms:4.1f}ms",
-                    f"{re.rate(self.last_time, now):.2f}/s",
-                )
-
-        if not final_summary:
-            table.add_row(
-                "Total",
-                str(total.count),
-                f"{total.errorcount} ({total.error_percentage:2.1f}%)",
-                f"{total.avg_ttlb_ms:4.1f}ms",
-                f"{total.max_ttlb:4.1f}ms",
-                f"{total.rate(self.last_time, now):.2f}/s",
-            )
-            self._console.print()
-        else:
-            table.title = "Summary"
-            total = RequestEntry()
-            for url, re in self.aggregate.items():
-                total += re
-                table.add_row(
-                    url,
-                    str(re.count),
-                    f"{re.errorcount} ({re.error_percentage:2.1f}%)",
-                    f"{re.avg_ttlb_ms:4.1f}ms",
-                    f"{re.max_ttlb_ms:4.1f}ms",
-                    f"{re.rate(self.start_time, now):.2f}/s",
-                )
-            table.add_row(
-                "Total",
-                str(total.count),
-                f"{total.errorcount} ({total.error_percentage:2.1f}%)",
-                f"{total.avg_ttlb_ms:4.1f}ms",
-                f"{total.max_ttlb_ms:4.1f}ms",
-                f"{total.rate(self.start_time, now):.2f}/s",
-            )
+            table.append(make_row(url, re, self.last_time, now))
+        table.append(make_row("Total", total, self.last_time, now))
 
         self.last_time = now
+
+        if not final_summary:
+            return table
+        else:
+            summary_total = RequestEntry()
+
+            for url, re in self.aggregate.items():
+                summary_total += re
+                summary_table.append(make_row(url, re, self.start_time, now))
+            summary_table.append(make_row("Total", summary_total, self.start_time, now))
+
+            return summary_table
+
+    def print_table(self, final_summary=False):
+        table = Table(show_edge=False)
+        table.add_column("Name", max_width=30)
+        table.add_column("Count", justify="right")
+        table.add_column("Failures", justify="right")
+        table.add_column("Avg", justify="right")
+        table.add_column("Max", justify="right")
+        table.add_column("Rate", justify="right")
+
+        for row in self.get_table(final_summary):
+            table.add_row(*row)
+
+        if final_summary:
+            table.title = "Summary"
 
         self._console.print(table)
 
         if final_summary and self.error_counter:
-            self._console.print()
             error_table = Table(show_edge=False)
-
             error_table.add_column("Count")
             error_table.add_column("Error")
 
             for key, count in sorted(self.error_counter.items(), key=lambda item: item[1], reverse=True):
                 error_table.add_row(str(count), key)
+
+            self._console.print()
             self._console.print(error_table)
 
-        return
