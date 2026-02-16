3eb305fd99c5cd1a7f3843c8dc535a46cb87bf56
Amadeu Pereira <amadeu.dx1@gmail.com>
2026-02-16 11:20:18 +0000

    Add worker class

--- src/aiolocust/runner.py
+++ src/aiolocust/runner.py
@@ -45,6 +45,29 @@ def distribute_evenly(total, num_buckets) -> list[int]:
     return [base + 1 if i < remainder else base for i in range(num_buckets)]
 
 
+class Worker:
+    def __init__(self, runner: Runner, user: Callable, stats: Stats):
+        self._runner = runner
+        self._user = user
+        self._stats = stats
+
+    async def _user_loop(self) -> None:
+        async with LocustClientSession(self._stats.request, self._runner) as client:
+            while self._runner.running:
+                try:
+                    await self._user(client)
+                except (ClientResponseError, AssertionError):
+                    pass
+
+    async def _run_users(self, user_count: int) -> None:
+        async with asyncio.TaskGroup() as tg:
+            for _ in range(user_count):
+                tg.create_task(self._user_loop())
+
+    def run(self, user_count: int) -> None:
+        asyncio.run(self._run_users(user_count), loop_factory=new_event_loop)
+
+
 class Runner:
     def __init__(self):
         signal.signal(signal.SIGINT, self.signal_handler)
@@ -67,22 +90,6 @@ class Runner:
         if self.stats.error_counter:
             self.console.print(self.stats.get_error_table())
 
-    async def user_loop(self, user):
-        async with LocustClientSession(self.stats.request, self) as client:
-            while self.running:
-                try:
-                    await user(client)
-                except (ClientResponseError, AssertionError):
-                    pass
-
-    async def user_runner(self, user, count):
-        async with asyncio.TaskGroup() as tg:
-            for _ in range(count):
-                tg.create_task(self.user_loop(user))
-
-    def thread_worker(self, user, count):
-        return asyncio.run(self.user_runner(user, count), loop_factory=new_event_loop)
-
     def signal_handler(self, _sig, _frame):
         print("\nStopping...")
         self.shutdown()
@@ -104,11 +111,12 @@ class Runner:
         loop = asyncio.get_running_loop()
         users_per_worker = distribute_evenly(user_count, event_loops)
 
-        coros = [asyncio.to_thread(self.thread_worker, user, i) for i in users_per_worker]
+        workers = [Worker(self, user, self.stats) for _ in range(event_loops)]
+
         loop.create_task(self.stats_printer())
 
         if duration:
             loop.call_later(duration, self.shutdown)
 
-        await asyncio.gather(*coros)
+        await asyncio.gather(*[asyncio.to_thread(w.run, n_users) for w, n_users in zip(workers, users_per_worker)])
         return
