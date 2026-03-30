00f733e60cdd52fcd401c78fa58bc6688a0d4f72
Lars Holmberg <lars.holmberg@redshirt.se>
2026-03-30 16:01:14 +0200

    update pw test

--- tests/test_pw.py
+++ tests/test_pw.py
@@ -1,28 +1,30 @@
-# trust me, this works, but it needs playwright to be installed and a browser to be downloaded
+import pytest
+from utils import assert_search
+
+from aiolocust.runner import Runner
+
+try:
+    from aiolocust.pw import PlaywrightUser
+except ImportError:
+    PlaywrightUser = object
 
 
-# from aiolocust.pw import PlaywrightUser
-# from aiolocust.runner import Runner
-# from tests.test_runner import WINDOWS_DELAY
-# from tests.utils import assert_search
+@pytest.mark.skipif(condition=PlaywrightUser is object, reason="Playwright is not installed")
+def test_runner(http_server, capteesys):  # noqa: ARG001
+    class TestUser(PlaywrightUser):  # pyright: ignore[reportGeneralTypeIssues]
+        async def run(self):
+            await self.page.goto("https://www.microsoft.com/")
+            await self.page.click("#uhfLogo > img", timeout=10000)
+            await self.page.click("this_doesnt_exist", timeout=10)
 
+    Runner([TestUser], 5, iterations=5).run_test()
+    out, err = capteesys.readouterr()
+    assert err == ""
+    assert "Summary" in out
+    assert_search(r" https://www.microsoft.com/[ ]+│[ ]+[5] .* \(0.0%\)", out)
 
-# async def test_runner(http_server, capteesys):  # noqa: ARG001
-#     class TestUser(PlaywrightUser):
-#         async def run(self):
-#             await self.page.goto("https://www.microsoft.com/")
-#             await self.page.click("#uhfLogo > img", timeout=10000)
-#             await self.page.click("this_doesnt_exist", timeout=10)
-
-#     r = Runner([TestUser])
-#     await r.run_test(1, 4 + WINDOWS_DELAY)
-#     out, err = capteesys.readouterr()
-#     assert err == ""
-#     assert "Summary" in out
-#     assert_search(r" https://www.microsoft.com/[ ]+│[ ]+[12] .* \(0.0%\)", out)
-
-#     assert_search(r" #uhfLogo > img[ ]+│[ ]+[12] .* \(0.0%\)", out)
-#     assert "Error" in out
-#     assert_search(r"[12] .* Page.click: Timeout", out)
-#     assert_search(r'waiting for locator\("this_doesnt_exist"\)', out)
-#     assert "bar" not in out
+    assert_search(r" #uhfLogo > img[ ]+│[ ]+[5] .* \(0.0%\)", out)
+    assert "Error" in out
+    assert_search(r"[5] .* Page.click: Timeout", out)
+    assert_search(r'waiting for locator\("this_doesnt_exist"\)', out)
+    assert "bar" not in out
