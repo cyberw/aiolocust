a80b1b351780a93c51027cbf1d59e00d807414c6
Lars Holmberg <lars.holmberg@redshirt.se>
2026-04-28 15:05:04 +0200

    Fix and generalize (use env vars) for mongodb example

--- examples/testdata_from_mongodb.py
+++ examples/testdata_from_mongodb.py
@@ -1,5 +1,7 @@
 # How to iterate over mongodb documents in LRU order, useful for getting test data (e.g. customers)
+import json
 import logging
+import os
 from collections.abc import AsyncIterator
 
 # Use threading.Lock because asyncio.Lock is not cross-loop safe
@@ -13,16 +15,15 @@ from pymongo.asynchronous.cursor import AsyncCursor
 from aiolocust import HttpUser, Runner
 
 logger = logging.getLogger(__name__)
+dblock = Lock()
 
 
 class MongoLRUReader(AsyncIterator[dict]):
     def __init__(self, coll: AsyncCollection, filter: dict | None = None, timestamp_field: str = "last_used"):
         self.timestamp_field = timestamp_field
         self.coll = coll
-        self.cursor: AsyncCursor = self.coll.find(filter or {}).sort(self.timestamp_field, pymongo.ASCENDING)
-        records_in_buffer = self.cursor._refresh()  # trigger fetch immediately instead of waiting for the first next()
-        if not records_in_buffer:
-            logger.warning(f"No records returned from query {filter}")
+        self.filter = filter or {}
+        self.cursor: AsyncCursor = self.coll.find(filter).sort(self.timestamp_field, pymongo.ASCENDING)
 
     async def __anext__(self) -> dict:
         with dblock:
@@ -31,7 +32,10 @@ class MongoLRUReader(AsyncIterator[dict]):
                 doc = await self.cursor.next()
             except StopAsyncIteration:
                 await self.cursor.rewind()
-                doc = await self.cursor.next()
+                try:
+                    doc = await self.cursor.next()
+                except StopAsyncIteration:
+                    raise Exception(f"cursor had no docs, maybe your filter ({self.filter}) is bad?")
             await self.coll.update_one(
                 {"_id": doc["_id"]},
                 {"$currentDate": {self.timestamp_field: True}},
@@ -39,16 +43,16 @@ class MongoLRUReader(AsyncIterator[dict]):
             return doc
 
 
-client = AsyncMongoClient("mongodb://localhost:27017")
-coll = client["sample_db"]["inventory"]
-mlr = MongoLRUReader(coll, {"qty": {"$gt": 1}})
-dblock = Lock()
+client = AsyncMongoClient(os.environ["MONGO_URI"])
+coll = client[os.environ["MONGO_DB"]][os.environ["MONGO_COLLECTION"]]
+filter = json.loads(os.getenv("MONGO_FILTER", "{}"))
+mlr = MongoLRUReader(coll, filter)
 
 
 class MyUser(HttpUser):
     async def run(self: HttpUser):
-        doc = await anext(mlr)
-        async with self.client.get(f"http://localhost:8080/{doc['item']}") as resp:
+        ssn = (await anext(mlr))["ssn"]
+        async with self.client.get(f"http://localhost:8080/{ssn}") as resp:
             pass
 
 
