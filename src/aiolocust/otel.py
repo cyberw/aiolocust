145b31b142ce4204eb5309d9cda97403826168ed
Lars Holmberg <lars.holmberg@redshirt.se>
2026-05-04 11:41:13 +0200

    Set up logging before otel, so log level configuration gets applied. Delay certain imports because otel is set up at import time.

--- src/aiolocust/otel.py
+++ src/aiolocust/otel.py
@@ -21,8 +21,7 @@ resource = Resource.create(
         "service.version": "0.0.0",  # __version__
     }
 )
-
-
+logger = logging.getLogger(__name__)
 logger_provider = LoggerProvider(resource=resource)
 set_logger_provider(logger_provider)
 tracer_provider = TracerProvider(resource=resource)
@@ -58,7 +57,7 @@ def setup_logging(level: int = logging.INFO):
                     or os.getenv("OTEL_EXPORTER_OTLP_PROTOCOL", "")
                 ):
                     print(
-                        f"OpenTelemetry otlp exporter for '{protocol}' is not available. Please install the required package: opentelemetry-exporter-otlp-proto-{'grpc' if protocol == 'grpc' else 'http'}",
+                        f"setup_logging: OpenTelemetry otlp exporter for '{protocol}' is not available. Please install the required package: opentelemetry-exporter-otlp-proto-{'grpc' if protocol == 'grpc' else 'http'}",
                     )
                 continue
 
@@ -95,7 +94,7 @@ def setup_logging(level: int = logging.INFO):
 
 
 def setup_trace_exporters():
-    logger = logging.getLogger(__name__)
+
     traces_exporters = {e.strip().lower() for e in os.getenv("OTEL_TRACES_EXPORTER", "otlp").split(",") if e.strip()}
     for exporter in traces_exporters:
         if exporter == "otlp":
@@ -120,7 +119,7 @@ def setup_trace_exporters():
                 level = logging.INFO if os.getenv("OTEL_TRACES_EXPORTER", "") else logging.DEBUG
                 logger.log(
                     level,
-                    f"OpenTelemetry otlp exporter for '{protocol}' is not available. Please install the required package: opentelemetry-exporter-otlp-proto-{'grpc' if protocol == 'grpc' else 'http'}",
+                    f"setup_trace_exporters: OpenTelemetry otlp exporter for '{protocol}' is not available. Please install the required package: opentelemetry-exporter-otlp-proto-{'grpc' if protocol == 'grpc' else 'http'}",
                 )
                 continue
 
@@ -158,32 +157,32 @@ def setup_meter_provider(metric_readers: list[MetricReader]):
                         OTLPMetricExporter,
                     )
                 else:
-                    print(
+                    logging.warning(
                         f"Unknown OpenTelemetry otlp meter exporter protocol '{protocol}'. Use 'grpc' or 'http/protobuf'"
                     )
                     continue
             except ImportError:
-                level = logging.INFO if os.getenv("OTEL_METRICS_EXPORTER", "") else logging.DEBUG
                 logger.log(
-                    level,
-                    f"OpenTelemetry otlp exporter for '{protocol}' is not available. Please install the required package: opentelemetry-exporter-otlp-proto-{'grpc' if protocol == 'grpc' else 'http'}",
+                    logging.INFO if os.getenv("OTEL_METRICS_EXPORTER", "") else logging.DEBUG,
+                    f"setup_meter_provider: OpenTelemetry otlp exporter for '{protocol}' is not available. Please install the required package: opentelemetry-exporter-otlp-proto-{'grpc' if protocol == 'grpc' else 'http'}",
                 )
                 continue
-
             metric_reader = PeriodicExportingMetricReader(OTLPMetricExporter())
             metric_readers.append(metric_reader)
 
         elif exporter == "prometheus":
-            print("Prometheus metrics exporter is not yet implemented!")
+            logger.warning("Prometheus metrics exporter is not yet implemented!")
 
         elif exporter == "console":
             metric_reader = PeriodicExportingMetricReader(ConsoleMetricExporter())
             metric_readers.append(metric_reader)
 
         elif exporter == "none":
-            continue
+            logger.debug("No metric reader configured, metrics will not be exported")
 
         else:
-            print(f"Unknown metrics exporter '{exporter}'. Ignored")
+            logger.warning(f"Unknown metrics exporter '{exporter}'. Ignored")
+    else:
+        logger.debug("No metrics exporter configured,")
 
     metrics.set_meter_provider(MeterProvider(resource=resource, metric_readers=metric_readers))
